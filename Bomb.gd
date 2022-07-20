8ad26eeff194315c7294b7fcd7716dfb9c4a8eb3
Liam <liamreadsspam@gmail.com>
2022-07-20 18:22:26 -0300

    Explosions

--- Bomb.gd
+++ Bomb.gd
@@ -4,15 +4,27 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var time = 1
+export(int) var time = 1
+export(Color) var color = Color.darkturquoise
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	print(self.position)
+	update()
+	
+func _draw():
+	draw_circle(Vector2.ZERO, 30, color)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time -= delta
 	if time <= 0:
-		queue_free()
+		explode()
+		if time <= -0.5:
+			queue_free()
+		
+func explode():
+	color = Color.transparent
+	get_node("Explosion").show()
+	update()
