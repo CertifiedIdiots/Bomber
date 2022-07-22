f775027271babb0fbd2e37c7161ca1237f36eb0b
Liam <liamreadsspam@gmail.com>
2022-07-22 11:52:38 -0300

    Remove print

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -6,9 +6,10 @@ func _physics_process(delta):
 	var player = get_parent().get_node("Player")
 	if player == null:
 		return
+		
 	var direction = (player.position - self.position).normalized()
-	print(direction)
 	move_and_slide(direction * speed)
+	
 	for i in get_slide_count():
 		var collision = get_slide_collision(i)
 		if collision.collider.name == "Player":
