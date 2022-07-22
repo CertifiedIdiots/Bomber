6c6a5ddc215e221d8b8be97d8d13f6a0e2415589
Liam <liamreadsspam@gmail.com>
2022-07-22 10:55:40 -0300

    Enemies move and kill player, bomb only hits enemies

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+export(int) var speed = 30
+
+func _physics_process(delta):
+	var player = get_parent().get_node("Player")
+	if player == null:
+		return
+	var direction = (player.position - self.position).normalized()
+	print(direction)
+	move_and_slide(direction * speed)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision.collider.name == "Player":
+			collision.collider.queue_free()
+		
+
+	
+	
--- Scripts/Explosion.gd
+++ Scripts/Explosion.gd
@@ -1,14 +1,6 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 export(Color) var color = Color.red
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass 
 
 func _draw():
 	draw_arc(Vector2.ZERO, 100, 0, TAU, 100, color)
@@ -16,7 +8,6 @@ func _draw():
 func _on_Timer_timeout():
 	queue_free()
 
-
 func _on_Explosion_body_entered(body):
-	if body is KinematicBody2D:
+	if body.name.begins_with("Enemy"):
 		body.queue_free()
