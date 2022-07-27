a1c0bf133dfe9f8dde5bff5dde3e525ac8b693e9
Liam <liamreadsspam@gmail.com>
2022-07-26 23:56:02 -0300

    Shooter cooldown

--- Scripts/ShooterEnemy.gd
+++ Scripts/ShooterEnemy.gd
@@ -6,7 +6,10 @@ func _ready():
 	self.attack_scene = preload("res://Objects/Projectile.tscn")
 
 func attack(target):
+	if stunned > 0: 
+		return
 	var instance:Node2D = self.attack_scene.instance()
 	instance.init(self, target)
 	instance.position = self.position
 	get_parent().add_child(instance)
+	self.stunned = 1.5
