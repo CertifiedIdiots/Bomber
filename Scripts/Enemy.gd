aa32f499fe63dec0d1532c3a29d64bb0aa60da09
cicetil <cicetil@gmail.com>
2022-07-30 16:47:37 -0400

    attack testing

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,12 +1,13 @@
 extends "Living.gd"
 
 export(int) var speed = 80
-var attack_range = 40
+var attack_range = 60
 var attack_cooldown = 0
-var dash_length = 20.0
+var dash_length = 100.0
 var last_dash = null
 var damaging = false
 var friction = 0.1
+var endpoint = null
 
 var attack_scene = preload("res://Objects/Attack.tscn")
 
@@ -37,27 +38,32 @@ func get_distance_to_player():
 func dash_attack():
 	var player = get_parent().get_node("Player")
 	var direction = player.global_position - self.global_position
-	var endpoint = self.position + direction.normalized() * dash_length
+	var direction_range = direction.normalized()
+	endpoint = self.position + direction.normalized() * dash_length
 	self.last_dash = self.position
 	damaging = true
-	$Tween.interpolate_property(self, "position", self.position, endpoint, 0.2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property(self, "position", self.position, endpoint, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	$Tween.start()
 
 func attack(target):
+	var player = get_parent().get_node("Player")
+	var direction = player.global_position - self.global_position
 	if attack_cooldown <= 0:
 		$Sprite.modulate = Color(1, 0, 0)
-		stunned = 1.0
-		yield(get_tree().create_timer(0.5), "timeout")
+		stunned = 1.5
+		yield(get_tree().create_timer(1.0), "timeout")
 		dash_attack()
-		attack_cooldown = rand_range(2.5, 5.0)
-
-func _on_Tween_tween_completed(object, key):
-	if last_dash != null:
-		$Tween.interpolate_property(self, "position", self.position, last_dash, 0.2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
-		$Tween.start()
+		attack_cooldown = rand_range(2.0, 4.0)
 		$Sprite.modulate = Color.white
-		last_dash = null
 		damaging = false
+
+#func _on_Tween_tween_completed(object, key):
+#	if last_dash != null:
+#		$Tween.interpolate_property(self, "position", self.position, last_dash, 0.2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+#		$Tween.start()
+#		$Sprite.modulate = Color.white
+#		last_dash = null
+#		damaging = false
 
 func _on_DamageShape_body_entered(body):
 	if body.is_in_group("player"):
